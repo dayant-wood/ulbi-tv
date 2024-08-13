d5b929e61c189bd2750f49ad32825ca5918ea12a
Галиева Диана <galievadi@sovcombank.ru>
2024-08-13 14:15:06 +0300

    feat: code splitting/lazy/suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, {Suspense} from "react";
+import {Link, Route, Routes} from "react-router-dom";
 import {Counter} from "./components/Counter";
 import './index.scss'
+import MainPage from "./pages/MainPage/MainPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
+import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
 export const App = () => {
 
-    return <div className="app">ghbdtn<Counter/></div>;
+    return <div className="app">
+        <Link to='/'>На главную</Link>
+        <Link to='/about'> На инфо о сайте</Link>
+
+        <Suspense fallback={<p>Loading....</p>}>
+            <Routes>
+            <Route path='/' element={<MainPageLazy/>}>Главная</Route>
+            <Route path='/about'  element={<AboutPageLazy/>}>О сайте</Route>
+        </Routes>
+        </Suspense>
+
+</div>;
 };
 
 export default App
---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AboutPage = () => { return <p>О сайте</p>}
+
+
+
+export default AboutPage;
