91e14afa5159d78e21083a973bb721d864ab0bc3
Галиева Диана <galievadi@sovcombank.ru>
2024-08-13 11:48:49 +0300

    feat: webpack dev server

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -5,8 +5,14 @@ export interface BuildPaths {
    build: string
    html: string
 }
+
+export type BuildEnv = {
+    port: number
+    mode: BuildMode
+}
 export interface BuildOptions {
     mode: BuildMode
     paths: BuildPaths
     isDev: boolean
+    port: number
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,19 +1,27 @@
 import path from "path";
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import {BuildPaths} from "./config/build/types/config";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
 
-const mode = 'development'
-const isDev = mode === 'development'
-const paths: BuildPaths = {
-   entry: path.resolve(__dirname, 'src', 'index.ts'),
-   build: path.resolve(__dirname,  'build'),
-   html: path.resolve(__dirname, 'public', 'index.html')
+
+
+
+export default (env: BuildEnv) => {
+    const mode = env.mode || 'development'
+    const isDev = mode === 'development'
+    const port = env.port ||  3000
+
+    const paths: BuildPaths = {
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        build: path.resolve(__dirname,  'build'),
+        html: path.resolve(__dirname, 'public', 'index.html')
+    }
+
+    const configuration = buildWebpackConfig({
+        mode,
+        paths,
+        isDev,
+        port
+    })
+
+    return configuration
 }
-
-const configuration = buildWebpackConfig({
-    mode,
-    paths,
-    isDev
-})
-
-export default configuration
